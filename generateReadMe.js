533885e6795dfa6bc0e6c13df3f18d8fe87fb040
bsaglimb <bsaglimb>
2023-12-06 02:25:12 -0500

    added functions to the readMe.js

--- generateReadMe.js
+++ generateReadMe.js
@@ -11,6 +11,56 @@ function renderContributingSection(confirmContributers, data) {
     }
   }
   
+   // Function that returns a license badge based on which license is choosen
+  // If there is no license, return an empty string
+  function renderLicenseBadge(license) { 
+    if (license !== 'no license') {
+      return `
+    ![badge](https://img.shields.io/badge/license-${license}-blue)
+      `;
+    } else {
+      return ' ';
+    }
+  }
+  
+  // Function that returns the license link
+  // If there is no license, return an empty string
+  function renderLicenseLink(license) {
+    if (license !== 'no license') {
+    return `
+    [${license}](https://choosealicense.com/licenses/${license})
+      `;
+    } else {
+      return ' ';
+    }
+  }
+  // Function that returns the license section of README
+  // If there is no license, return an empty string
+  function renderLicenseSection(license) {
+    if (license !== 'no license') {
+    return `
+    ## [License](#table-of-contents)
+  
+    The application is covered under the following license:
+  
+    ${renderLicenseLink(license)}
+      `;
+    } else {
+      return ' ';
+    }
+   }
+  
+   // Function that returns license in table of contents
+   // If there is no license, return an empty string
+  function renderLicenseTOC(license) {
+    if (license !== 'no license') {
+    return `
+    * [License](#license)
+      `;
+    } else {
+      return ' ';
+    }
+   }
   
   
   module.exports = generateReadMe;
