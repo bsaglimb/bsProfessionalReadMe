19eacfa2c3246a83c559f24b097334ce853abe71
bsaglimb <bsaglimb>
2023-12-06 02:00:59 -0500

    added to readMe.js

---
+++ generateReadMe.js
@@ -0,0 +1,16 @@
+// Function to return a message if user doesn't want contributors
+function renderContributingSection(confirmContributers, data) {
+    if (!confirmContributers) {
+      return `
+    Thank you for your interest in helping out; however, I will not be accepting contributions from third parties.
+      `;
+    } else {
+      return `
+    ${data}
+      `;
+    }
+  }
+  
+  
+  
+  module.exports = generateReadMe;
